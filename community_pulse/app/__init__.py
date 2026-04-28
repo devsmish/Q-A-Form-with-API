3f0da972f3637bec77f9a5015373f83a8df75dd4
devsmish <smyshynskyi@gmail.com>
2026-04-28 22:24:45 +0200

    feat: setup Flask app with SQLAlchemy, models (Question, Response, Statistic), and basic API routes

---
+++ community_pulse/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from community_pulse.app.routers.rout_quest import questions_bp
+from community_pulse.app.routers.rout_resp import responses_bp
+from community_pulse.config import DevelopmentConfig
+from community_pulse.app.extensions import db, migrate
+import community_pulse.app.models
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    app.register_blueprint(questions_bp, url_prefix='/questions')
+    app.register_blueprint(responses_bp, url_prefix='/responses')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
